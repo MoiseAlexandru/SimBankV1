9f0c5c0b2e60f9360e803e2c987e2c5e25aad975
MoiseAlexandru <moise.alexandru.ichb@gmail.com>
2023-04-02 16:36:44 +0300

    Adaugat clasa Card

--- src/Card.java
+++ src/Card.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class Card {
+import java.time.LocalDate;
+import java.util.Date;
+
+public class Card {
+    private String cardNumber;
+    private int CVV;
+    private String ownerId;
+    private Date cardCreationDate;
+    private LocalDate cardExpirationDate;
+    private boolean cardActivationStatus;
+    public Card() {}
+    public void activateCard() {
+        this.cardActivationStatus = true;
+    }
+    public void deactivateCard() {
+        this.cardActivationStatus = false;
+    }
+    public boolean expired() {
+        LocalDate today = LocalDate.now();
+        if(today.isAfter(cardExpirationDate))
+            return true;
+        return false;
+    }
 }
