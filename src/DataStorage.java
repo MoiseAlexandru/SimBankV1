fe616a8a7def250fa6567aabbd8ded7833ea5833
MoiseAlexandru <moise.alexandru.ichb@gmail.com>
2023-04-07 19:00:55 +0300

    Adaugat clasa DataStorage

--- src/DataStorage.java
+++ src/DataStorage.java
@@ -1,2 +1,12 @@
+import java.util.ArrayList;
+
 public class DataStorage {
+    public static ArrayList <User> Users = new ArrayList <User>();
+    public static ArrayList <Card> Cards = new ArrayList <Card>();
+    public static ArrayList <Account> Accounts = new ArrayList <Account>();
+    public static ArrayList <Transaction> Transactions = new ArrayList <Transaction>();
+    public static int numUsers = 0;
+    public static int numCards = 0;
+    public static int numAccounts = 0;
+    public static int numTransactions = 0;
 }
