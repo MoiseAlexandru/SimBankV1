c675fdab2588ef1f590462bf385586cb6a3d948c
MoiseAlexandru <moise.alexandru.ichb@gmail.com>
2023-04-07 19:42:50 +0300

    Adaugat UserService, UserRepository

--- src/UserRepository.java
+++ src/UserRepository.java
@@ -1,2 +1,36 @@
-package PACKAGE_NAME;public class UserRepository {
+import javax.xml.crypto.Data;
+
+public class UserRepository {
+    public void deleteUserById(String userId) throws IllegalArgumentException {
+        User user = getUserById(userId);
+        if(user == null)
+            throw new IllegalArgumentException("Error in function deleteByUserId of class UserRepository: user not found");
+        DataStorage.Users.remove(user);
+    }
+    public void deleteUserByName(String name) throws IllegalArgumentException {
+        User user = getUserByName(name);
+        if(user == null)
+            throw new IllegalArgumentException("Error in function deleteByUserId of class UserRepository: user not found");
+        DataStorage.Users.remove(user);
+    }
+    public User getUserById(String userId) {
+        for(User user : DataStorage.Users) {
+            if(user.userId == userId)
+                return user;
+        }
+        return null;
+    }
+    public User getUserByName(String name) {
+        for(User user : DataStorage.Users) {
+            if(user.name == name)
+                return user;
+        }
+        return null;
+    }
+    public void addUser(User user) throws IllegalArgumentException {
+        if(getUserById(user.userId) != null) {
+            throw new IllegalArgumentException("Error in addUser of UserRepository class: User already exists in the database!");
+        }
+        DataStorage.Users.add(user);
+    }
 }
--- src/UserService.java
+++ src/UserService.java
@@ -1,2 +1,46 @@
-package PACKAGE_NAME;public class UserService {
+import java.time.LocalDate;
+
+public class UserService {
+    private UserRepository userRepository;
+    private CardRepository cardRepository;
+    private AccountRepository accountRepository;
+    public UserService(UserRepository userRepository, CardRepository cardRepository, AccountRepository accountRepository) {
+        this.userRepository = userRepository;
+        this.cardRepository = cardRepository;
+        this.accountRepository = accountRepository;
+    }
+    public void createUser(String name, String address, String nationality, long identityCard, String phoneNumber, String occupation, LocalDate birthdate, String gender, boolean married) throws IllegalArgumentException{
+        UserProfile user = new UserProfile(name, address, nationality, identityCard, phoneNumber, occupation, birthdate, gender, married);
+        try {
+            userRepository.addUser(user);
+        }
+        catch(Exception err) {
+            throw new IllegalArgumentException("Error in function createUser of userRepository", err);
+        }
+    }
+    public void createUser(String name, String address, String contactPerson, String contactPhone, String industry, int companySizeMin, int companySizeMax) throws IllegalArgumentException{
+        BusinessProfile user = new BusinessProfile(name, address, contactPerson, contactPhone, industry, companySizeMin, companySizeMax);
+        try {
+            userRepository.addUser(user);
+        }
+        catch(Exception err) {
+            throw new IllegalArgumentException("Error in function createUser of userRepository", err);
+        }
+    }
+    public void deleteUserById(String userId) throws IllegalArgumentException{
+        try {
+            userRepository.deleteUserById(userId);
+        }
+        catch(Exception err) {
+            throw new IllegalArgumentException("Error in function deleteUserById of class UserService", err);
+        }
+    }
+    public void deleteUserByName(String name) {
+        try {
+            userRepository.deleteUserByName(name);
+        }
+        catch(Exception err) {
+            throw new IllegalArgumentException("Error in function deleteUserByName of class UserService", err);
+        }
+    }
 }
