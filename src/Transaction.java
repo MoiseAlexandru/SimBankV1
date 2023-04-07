2f02ca0a16c4d19f6a4dd3c21a98d3b08b7cb616
MoiseAlexandru <moise.alexandru.ichb@gmail.com>
2023-04-07 17:21:44 +0300

    adaugare clasa Transaction

--- src/Transaction.java
+++ src/Transaction.java
@@ -1,5 +1,41 @@
+import java.util.Date;
+import java.util.UUID;
+
 public class Transaction {
     String transactionId;
     String senderAccount;
     String receiverAccount;
+    double amount;
+    Date transactionDate;
+    String description;
+    int transactionType;
+    // Tip 1: retragere bani
+    // Tip 2: alimenentare cont
+    // Tip 3: intre doua conturi
+    Transaction() {}
+    Transaction(int type, double amount, String account) {
+        this.transactionId = UUID.randomUUID().toString();
+        this.transactionDate = new Date();
+        this.amount = amount;
+        this.transactionType = type;
+        if(type == 1) {
+            this.description = "Retragere din cont";
+            this.senderAccount = account;
+            this.receiverAccount = null;
+        }
+        if(type == 2) {
+            this.senderAccount = null;
+            this.receiverAccount = account;
+            this.description = "Alimentare cont";
+        }
+    }
+    Transaction(double amount, String senderId, String receiverId, String description) {
+        this.transactionId = UUID.randomUUID().toString();
+        this.transactionDate = new Date();
+        this.senderAccount = senderId;
+        this.receiverAccount = receiverId;
+        this.amount = amount;
+        this.transactionType = 3;
+        this.description = description;
+    }
 }
